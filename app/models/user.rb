751b88ecc19034e517dc9ea0fc177ab65b46117c
Rodrigo <rodrigo.lindenmeyer.toledo@gmail.com>
2020-01-11 12:30:12 -0300

    add cancancan with admin and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_initialize do
+    if self.new_record?
+      self.role ||= :visit
+    end
+  end
+
+  enum role: [ :admin, :visit]
 end
